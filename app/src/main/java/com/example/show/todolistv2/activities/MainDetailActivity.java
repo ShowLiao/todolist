aecd04ba02482195ebd9af4f5cc091b2281f4c97
Show Liao <u8n.show@gmail.com>
2017-08-17 12:25:46 -0700

    improve dialogfragment and listview

---
+++ app/src/main/java/com/example/show/todolistv2/activities/MainDetailActivity.java
@@ -0,0 +1,15 @@
+package com.example.show.todolistv2.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.show.todolistv2.R;
+
+public class MainDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_detail);
+    }
+}
